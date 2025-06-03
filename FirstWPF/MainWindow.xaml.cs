dca7f374a331d6947022a609810d1a69f2633011
CavidHuseinov <Cavid.huseynovv28@gmail.com>
2025-06-03 14:18:57 +0400

    add exit,maximized,minimalized btn and double click max-min screen code

--- FirstWPF/MainWindow.xaml.cs
+++ FirstWPF/MainWindow.xaml.cs
@@ -25,5 +25,45 @@ namespace FirstWPF
         {
                 
         }
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+        private void Exit_Btn(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void DoubleClickScreen(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                if (WindowState == WindowState.Maximized)
+                {
+                    WindowState = WindowState.Normal;
+                }
+                else
+                    WindowState = WindowState.Maximized;
+                e.Handled = true;
+            }
+        }
+        private void Maximized_Btn(object sender, RoutedEventArgs e)
+        {
+            if(WindowState == WindowState.Maximized)
+            {
+                WindowState = WindowState.Normal;
+            }
+            else
+            {
+                WindowState = WindowState.Maximized;
+
+            }
+        }
+        private void Minimize_Btn(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
     }
 }
